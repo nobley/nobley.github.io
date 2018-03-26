07d01e03780621fb30c72614fee6de7bdcf4a3ee
nobley <36251450+nobley@users.noreply.github.com>
2018-03-26 05:14:50 +0100

    home page improved

---
+++ javascript/agulu_1.js
@@ -0,0 +1,31 @@
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1} 
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+function myMap() {
+  var myCenter = new google.maps.LatLng(6.117203, 7.039090);
+  var mapCanvas = document.getElementById("map");
+  var mapOptions = {center: myCenter, zoom: 10};
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+  var marker = new google.maps.Marker({position:myCenter});
+  marker.setMap(map);
+}
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "75%";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
